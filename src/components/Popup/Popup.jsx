0e0cdfca553c917bbec06d555245b3fe2e1af955
MirasGitHub <121196122+MirasGitHub@users.noreply.github.com>
2023-06-03 18:21:22 +0400

    create popup component that displays through the Results.jsx on Try Again button click

---
+++ src/components/Popup/Popup.jsx
@@ -0,0 +1,85 @@
+import { useEffect, useRef } from "react";
+import { Button } from "reactstrap";
+
+import "./Popup.css";
+
+const Popup = ({
+	handleClose,
+	score,
+	refreshPage,
+	quizResults,
+	setQuizResults,
+	currentResult,
+	setCurrentResult,
+}) => {
+	const popupRefWrapper = useRef(null);
+
+	const popupRef = useRef(null);
+
+	useEffect(() => {
+		const handleClick = (e) => {
+			if (popupRef.current && popupRefWrapper.current) {
+				if (
+					popupRefWrapper.current.contains(e.target) &&
+					!popupRef.current.contains(e.target)
+				) {
+					handleClose();
+				}
+			}
+		};
+
+		document.addEventListener("click", handleClick);
+
+		return () => {
+			document.removeEventListener("click", handleClick);
+		};
+	});
+
+	//Load quiz results from localStorage on component mount
+	useEffect(() => {
+		const storedResults = localStorage.getItem("Results");
+		if (storedResults) {
+			setQuizResults(JSON.parse(storedResults));
+		}
+
+		console.log("stored results", storedResults);
+	}, [setQuizResults]);
+
+	const handleSave = (answer) => {
+		if (answer === "Yes") {
+			if (currentResult) {
+				const updatedResults = [...quizResults, currentResult];
+				localStorage.setItem("Results", JSON.stringify(updatedResults));
+			}
+			const currentQuizResult = {
+				points: score,
+				total: 9,
+				date: new Date(),
+			};
+			setCurrentResult(currentQuizResult);
+
+			setTimeout(() => {
+				refreshPage();
+			}, 100);
+		} else if (answer === "No") {
+			refreshPage();
+		}
+	};
+
+	return (
+		<div className="popup-wrapper" ref={popupRefWrapper}>
+			<div className="popup" ref={popupRef}>
+				<h3>Do you want to save this attempt? </h3>
+				<br />
+				<Button color="success" onClick={() => handleSave("Yes")}>
+					Yes
+				</Button>
+				<Button color="danger" onClick={() => handleSave("No")}>
+					No
+				</Button>
+			</div>
+		</div>
+	);
+};
+
+export default Popup;
