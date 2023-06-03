ecbb1420e5407dc2f95541c7dda07568725b25e2
MirasGitHub <121196122+MirasGitHub@users.noreply.github.com>
2023-06-03 18:28:00 +0400

    adding the files from other branches

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes, useNavigate } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import Quiz from "./components/Quiz/Quiz";
+import Results from "./components/Results";
+import { useEffect, useState } from "react";
+import History from "./components/History/History";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [score, setScore] = useState(0);
+	const [quizResults, setQuizResults] = useState([]);
+	const [currentResult, setCurrentResult] = useState(null);
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const storedResults = localStorage.getItem("Results");
+		if (storedResults) {
+			setQuizResults(JSON.parse(storedResults));
+		}
+	}, [setQuizResults]);
+
+	useEffect(() => {
+		const storedResults = localStorage.getItem("Results");
+		if (storedResults) {
+			setQuizResults(JSON.parse(storedResults));
+		}
+
+		if (storedResults) {
+			const parsedResults = JSON.parse(storedResults);
+
+			const sortedResults = parsedResults.sort((a, b) => {
+				if (a.points !== b.points) {
+					return b.points - a.points;
+				}
+
+				return new Date(b.date) - new Date(a.date);
+			});
+
+			setQuizResults(sortedResults);
+		}
+	}, []);
+
+	const refreshPage = () => {
+		setScore(0);
+		navigate("/");
+	};
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/" exact element={<Home />} />
+				<Route
+					path="quiz"
+					element={
+						<Quiz score={score} setScore={setScore} refreshPage={refreshPage} />
+					}
+				/>
+				<Route
+					path="results"
+					element={
+						<Results
+							score={score}
+							refreshPage={refreshPage}
+							quizResults={quizResults}
+							setQuizResults={setQuizResults}
+							currentResult={currentResult}
+							setCurrentResult={setCurrentResult}
+						/>
+					}
+				/>
+				<Route
+					path="history"
+					element={
+						<History
+							quizResults={quizResults}
+							setQuizResults={setQuizResults}
+							currentResult={currentResult}
+							setCurrentResult={setCurrentResult}
+							score={score}
+							setScore={setScore}
+							refreshPage={refreshPage}
+						/>
+					}
+				/>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
