5259fcf3617355708b882e7a58684f95962bfe23
MirasGitHub <121196122+MirasGitHub@users.noreply.github.com>
2023-06-03 18:16:39 +0400

    create results page in order to display quiz final score

---
+++ src/components/Results.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+import Popup from "./Popup/Popup";
+import { Button } from "reactstrap";
+import { useNavigate } from "react-router-dom";
+
+const Results = ({
+	score,
+	refreshPage,
+	currentResult,
+	setCurrentResult,
+	quizResults,
+	setQuizResults,
+}) => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const navigate = useNavigate();
+
+	const handleRouteToHistory = () => {
+		navigate("/history");
+	};
+
+	return (
+		<div className="results-container">
+			<h4>
+				<br />
+				Final Score: {score} / 9.
+			</h4>
+			<br />
+			{isOpen && (
+				<Popup
+					refreshPage={refreshPage}
+					score={score}
+					handleClose={() => setIsOpen(!isOpen)}
+					quizResults={quizResults}
+					setQuizResults={setQuizResults}
+					currentResult={currentResult}
+					setCurrentResult={setCurrentResult}
+				/>
+			)}
+			<br />
+			<Button color="primary" onClick={() => setIsOpen(!isOpen)}>
+				Try again
+			</Button>
+			<br />
+			<br />
+			<Button color="info" onClick={handleRouteToHistory}>
+				See Attempts History
+			</Button>
+		</div>
+	);
+};
+
+export default Results;
