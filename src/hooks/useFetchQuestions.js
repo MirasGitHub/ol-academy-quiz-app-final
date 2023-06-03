a0678434567598912ccc309ea8541ad83ec08e49
MirasGitHub <121196122+MirasGitHub@users.noreply.github.com>
2023-06-03 17:57:45 +0400

    create useFetchQuestion.js file for fetching the questions from api

---
+++ src/hooks/useFetchQuestions.js
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+
+const DATA_URL = `https://my-json-server.typicode.com/MirasGitHub/api-for-ol-academy-final-project/db`;
+
+const useFetchQuestions = () => {
+	const [questions, setQuestions] = useState();
+
+	const EXPIRY = 10 * 60 * 1000;
+
+	useEffect(() => {
+		const fetchFromLocalStorage = () => {
+			const storedData = localStorage.getItem("questions");
+			const storedTimestamp = localStorage.getItem("questionsTimestamp");
+			const currentTime = Date.now();
+
+			if (
+				storedData &&
+				storedTimestamp &&
+				currentTime - storedTimestamp < EXPIRY
+			) {
+				return JSON.parse(storedData);
+			}
+
+			return null;
+		};
+
+		const fetchQuestions = async () => {
+			const storedData = fetchFromLocalStorage();
+
+			if (storedData) {
+				setQuestions(storedData);
+			} else {
+				try {
+					const response = await fetch(DATA_URL);
+					const jsonData = await response.json();
+					setQuestions(jsonData);
+
+					saveDataToLocalStorage(jsonData);
+				} catch (error) {
+					console.error("Error fetching data:", error);
+				}
+			}
+		};
+
+		fetchQuestions();
+	}, [EXPIRY]);
+
+	const saveDataToLocalStorage = (data) => {
+		localStorage.setItem("questions", JSON.stringify(data));
+		localStorage.setItem("questionsTimestamp", Date.now().toString());
+	};
+
+	return {
+		questions,
+	};
+};
+
+export default useFetchQuestions;
